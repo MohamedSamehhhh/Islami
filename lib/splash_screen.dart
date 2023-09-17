961c2bee29b83497c628a369f3153d8d45c32d1b
MohamedSamehhhh <mohamedsameh7222@gmail.com>
2023-09-17 20:39:14 +0300

    change to minute

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,7 +6,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(
-      Duration(seconds: 10),
+      Duration(minutes: 10),
       () {
         Navigator.pushReplacementNamed(context, SplashScreen.routeName);
       },

872711550c44fd78c85ad03ee958ed25eee7c334
MohamedSamehhhh <mohamedsameh7222@gmail.com>
2023-09-17 21:04:41 +0300

    change to mili

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,7 +6,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(
-      Duration(minutes: 10),
+      Duration(milliseconds: 10),
       () {
         Navigator.pushReplacementNamed(context, SplashScreen.routeName);
       },
